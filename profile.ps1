a14fc93f84545c1b20156f537baff15fd20d67f0
Barret Rennie <barret@brennie.ca>
2014-07-23 08:46:28 -0600

    Add profile

---
+++ profile.ps1
@@ -0,0 +1,28 @@
+function IsAdministrator {
+    $user = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+    return (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+$global:HostName = (hostname)
+$global:IsAdministrator = IsAdministrator
+
+function global:prompt
+{
+    if ($global:IsAdministrator)
+    {
+        Write-Host -NoNewline $env:UserName -ForegroundColor Red
+    }
+    else
+    {
+        Write-Host -NoNewline $env:UserName -ForegroundColor White
+    }
+
+    Write-Host -NoNewline "@"
+    Write-Host -NoNewline $global:HostName -ForegroundColor White
+    Write-Host -NoNewline " "
+    Write-Host -NoNewline (Get-Location.Path -ForegroundColor White)
+
+    return "> "
+}
+
+Set-Alias subl "C:\Program Files\Subliem Text 3\sublime_text.exe"
